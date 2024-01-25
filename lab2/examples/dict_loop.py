00fa7cced9f11499448082ff892d091925bbe0d1
yor <46350522+omnious-potato@users.noreply.github.com>
2024-01-25 16:46:47 +0600

    Added remaining examples for Lab2

---
+++ lab2/examples/dict_loop.py
@@ -0,0 +1,16 @@
+thisdict = {}
+
+for x in thisdict:
+    print(x)
+
+for x in thisdict:
+    print(thisdict[x])
+
+for x in thisdict.values():
+    print(x)
+
+for x in thisdict.keys():
+    print(x)
+
+for x, y in thisdict.items():
+    print(x, y)
