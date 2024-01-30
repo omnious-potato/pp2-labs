5f10d80b512c6d49c68ce8c2dd7ef9cf6f90b64b
yor <46350522+omnious-potato@users.noreply.github.com>
2024-01-30 14:02:11 +0600

    Added remaining tasks in functions1

--- lab3/tasks/functions1/5.py
+++ lab3/tasks/functions1/5.py
@@ -1,7 +1,13 @@
-#TODO
-def printAllPermutations(src):
-    pass
+def permute(src, sz = 0):
+    if sz == len(src):
+        print("".join(src))
+    
 
+    for i in range(sz, len(src)):
+        temp = [ch for ch in src]
+        temp[sz], temp[i]= temp[i], temp[sz] #swap
 
+        permute(temp, sz + 1)
+        
 s = input()
-printAllPermutations(s)
+permute(s)
